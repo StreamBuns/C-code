eaf2554c0173453ed36544989e5860b8647f8a00
StreamBuns <flyingpig@nwafu.edu.cn>
2019-05-01 23:10:44 +0800

    Test using namespace std

--- AcquianticeC++/AcquianticeC++/main.cpp
+++ AcquianticeC++/AcquianticeC++/main.cpp
@@ -7,9 +7,9 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "My name is Tony\n";
+    cout << "My name is Tony\n";
     return 0;
 }
