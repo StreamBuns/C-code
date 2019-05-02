c1d6e2b2cf67825c54cc25c2902945d388954498
StreamBuns <flyingpig@nwafu.edu.cn>
2019-05-02 11:48:34 +0800

    3.14acquanintice  cin and cout

--- AcquianticeC++/AcquianticeC++/main.cpp
+++ AcquianticeC++/AcquianticeC++/main.cpp
@@ -10,11 +10,31 @@
 using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int Number1 = 0,Number2 = 0;
-    cout <<"Enter two numbers: "<<endl;
-    cin  >> Number1 >> Number2;
-    cout << "The sum of " << Number1 << " and "<< Number2 <<" is "
-         << Number1 + Number2 << endl;
+    cout << true << "\t" << false << endl;
+    
+    cout << boolalpha << true << "\n" << false << endl;
+    cout << noboolalpha;
+    cout << true << "\t" << false;
+    
+    //char Name [20]={'\0'};
+//    int i;
+//    for ( i = 0; i < 20 ;i++ )
+//    {
+//        Name[i] = cin.get() ;
+//        if (Name[i] == '\n')
+//            break;
+//    }
+//    for ( i = 0; i < 20 ;i++ )
+//    {
+//        cout.put(Name[i]);
+//        if (Name[i] == '\n')
+//            break;
+//    }
+////    int Number1 = 0,Number2 = 0;
+//    cout <<"Enter two numbers: "<<endl;
+//    cin  >> Number1 >> Number2;
+//    cout << "The sum of " << Number1 << " and "<< Number2 <<" is "
+//         << Number1 + Number2 << endl;
 //    cout << "My name is Tony\n";
     return 0;
 }
